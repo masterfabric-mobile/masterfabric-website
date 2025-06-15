b910d6ab550887267c5bb1b8996c2a9c22f6b181
SelimKutuk <selimmmm1525@gmail.com>
2025-06-15 20:50:44 +0300

    fix: astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
+import path from "path";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://masterfabric.co",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
+  alias: {
+    "@layouts": path.resolve("./src/layouts"),
+  },
 });

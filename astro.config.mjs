da6fd65240205fde16f68ea5770b5458e7425c92
Nisa <76876477+nisacolak@users.noreply.github.com>
2025-06-18 19:45:39 +0300

    update: enhance configuration and add dev:silent script for cleaner output

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,27 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
+import vercel from "@astrojs/vercel/serverless";
+import path from "path";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://masterfabric.co",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
+   output: "server", 
+  adapter: vercel(),
+
+  devToolbar: {
+    enabled: false, 
+  },
+
+  vite: {
+    resolve: {
+      alias: {
+        "@layouts": path.resolve("./src/layouts"),
+        "@components": path.resolve("./src/components"), 
+        "@pages": path.resolve("./src/pages"),
+      },
+    },
+  },
 });
